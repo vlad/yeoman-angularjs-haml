dbeca9ab1c5485ca474268a0879ec1dfe5aab493
Vlad Yazhbin <yazhbin@gmail.com>
2012-10-07 19:52:13 -0700

    initial state after running "yeoman init angular"

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exampleApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ app/scripts/example.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var exampleApp = angular.module('exampleApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
